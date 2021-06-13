d301e5a066e548857642a8f8b97f0e48759ae76c
Laurent <l0lo86@free.fr>
2021-06-13 21:07:09 +0200

    update to 19.4 for build test

--- Assets/Demo/Scripts/utilities/LODGroupOverride.cs
+++ Assets/Demo/Scripts/utilities/LODGroupOverride.cs
@@ -3,20 +3,88 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class LODGroupOverride : MonoBehaviour {
 
-    public float lodSize = 1.0f;
-    public Vector3 localReference = Vector3.zero;
+    public float customSize = 1.0f;
+    public Vector3 customReferencePoint = Vector3.zero;
 
-	void OnStart () {
-        GetComponent<LODGroup>().size = lodSize;
-        GetComponent<LODGroup>().localReferencePoint = localReference;
+    private LODGroup lodGroup;
+
+    public bool overrideSize;
+    public bool overrideReferencePoint;
+    public bool shareLightmapsAcrossLODs;
+
+    void OnStart()
+    {
+        GetLODGroup(this.gameObject);
+
+        SetLODGroupOverrides(lodGroup);
+    }
+
+    private void GetLODGroup(GameObject gameObject)
+    {
+        if (lodGroup == null)
+            lodGroup = gameObject.GetComponent<LODGroup>();
+    }
+
+    private void SetLODGroupOverrides(LODGroup group)
+    {
+        if (group == null)
+            return;
+        if(overrideSize)
+            group.size = customSize;
+        if(overrideReferencePoint)
+            group.localReferencePoint = customReferencePoint;
+    }
+
+// Get mesh renderer on host
+    private void Awake()
+    {
+        if(shareLightmapsAcrossLODs)
+        {
+            GetLODGroup(this.gameObject);
+            RendererInfoTransfer(lodGroup);
+        }
     }
 
 #if UNITY_EDITOR
-    void Update () {
-        if (GetComponent<LODGroup>().size != lodSize)
-            GetComponent<LODGroup>().size = lodSize;
-        if (GetComponent<LODGroup>().localReferencePoint != localReference)
-            GetComponent<LODGroup>().localReferencePoint = localReference;
+    private void OnEnable()
+    {
+        GetLODGroup(this.gameObject);
+        SetLODGroupOverrides(lodGroup);
     }
 #endif
+
+    void RendererInfoTransfer(LODGroup group)
+    {
+        if (group == null)
+            return;
+
+        //Gather LODs
+        var lods = group.GetLODs();
+
+        //Apply settings from LOD0 to all LODs
+        for (int i = 1; i < lods.Length; i++)
+        {
+            var renderers = lods[i].renderers;
+
+            for (int j = 0; j < renderers.Length; j++)
+            {
+                if (renderers[j] != null)
+                {
+                    try
+                    {
+                        renderers[j].lightProbeUsage = lods[0].renderers[j].lightProbeUsage;
+                        renderers[j].lightmapIndex = lods[0].renderers[j].lightmapIndex;
+                        renderers[j].lightmapScaleOffset = lods[0].renderers[j].lightmapScaleOffset;
+                        renderers[j].realtimeLightmapIndex = lods[0].renderers[j].realtimeLightmapIndex;
+                        renderers[j].realtimeLightmapScaleOffset = lods[0].renderers[j].realtimeLightmapScaleOffset;
+                    }
+                    catch
+                    {
+                        if (Debug.isDebugBuild)
+                            Debug.Log("Lightmapped LOD : Error setting lightmap settings on " + gameObject.name);
+                    }
+                }
+            }
+        }
+    }
 }
